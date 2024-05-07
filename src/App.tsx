4b883ea703753833df0952085a9b02d41628231b
ACoolmanTelicent <a.coleman@telicent.io>
2024-05-07 10:00:56 +0100

    update(tailwind): move to public/indexhtml

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { TeliStandardLayout } from "@telicent-oss/ds";
 import './App.css';
 import '@telicent-oss/ds/dist/style.css';
 import "@telicent-oss/ds/dist/fontawesome.css";
-import './main.css'
 import { RdfInstanceViewer } from './rdfInstanceViewer/RdfInstanceViewer'
 import { HierarchyMenu } from "./Components/HierarchyMenu/HierarchyMenu";
 import { APP_CONFIG_JSON } from "./constants";
