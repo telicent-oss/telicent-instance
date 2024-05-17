0314d6cbf4e173ffb126241f57b8099dafc8c119
Dave Cockrell <dave@telicent.io>
2024-05-18 00:44:40 +0100

    ci: fixup config and docker build
    WindiCSS is being sunset so swiched back to tailwindCSS
    
    updated docker build to match new config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
-import WindiCSS, { exclude } from 'vite-plugin-windicss'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), WindiCSS()],
+  plugins: [react()],
   test: {
     environment: "jsdom",
   }
