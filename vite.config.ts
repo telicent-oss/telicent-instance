f9a864a7fd80a49fc65c99fb76519c6f7d65a475
Dave Cockrell <dave@telicent.io>
2024-05-17 16:40:14 +0100

    fix: remove owasp workflows
    remove owasp workflow until it is available in the shared workflows
    remove include / exclude lines as didn't add anything to the test
    output

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,5 @@ export default defineConfig({
   plugins: [react(), WindiCSS()],
   test: {
     environment: "jsdom",
-    include: ["./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    exclude: [
-      "src/public/**",
-      "**/node_modules/**",
-    ]
   }
 })
